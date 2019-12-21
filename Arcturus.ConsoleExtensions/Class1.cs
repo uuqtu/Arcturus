7e43e9097dc964528339ac80e69f6bebdc057551
Tria <Tria@TRIA-PC>
2019-12-21 18:18:22 +0100

    Projektdateien hinzufügen.

---
+++ Arcturus.ConsoleExtensions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcturus.ConsoleExtensions
+{
+    public class Class1
+    {
+    }
+}
