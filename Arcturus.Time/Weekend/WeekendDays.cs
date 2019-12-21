70aa00cfed18aa13dc928ef7b090f37a85d244b9
Tria <Tria@TRIA-PC>
2019-12-21 23:27:59 +0100

    Added Weektime classes.

---
+++ Arcturus.Time/Weekend/WeekendDays.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcturus.Time.Weekend
+{
+    public static class WeekendDays
+    {
+        public static List<DateTime> GetWeekendDays(int year)
+        {
+            var weekends = GetDaysBetween(new DateTime(1, 1, year), new DateTime(31, 12, year))
+                .Where(d => d.DayOfWeek == DayOfWeek.Saturday || d.DayOfWeek == DayOfWeek.Sunday);
+
+            return weekends.ToList();
+        }
+
+        public static List<DateTime> GetWeekendDays(DateTime start, DateTime end)
+        {
+            var weekends = GetDaysBetween(start, end)
+                .Where(d => d.DayOfWeek == DayOfWeek.Saturday || d.DayOfWeek == DayOfWeek.Sunday);
+
+            return weekends.ToList();
+        }
+
+        public static IEnumerable<DateTime> GetDaysBetween(DateTime start, DateTime end)
+        {
+            for (DateTime i = start; i < end; i = i.AddDays(1))
+            {
+                yield return i;
+            }
+        }
+
+
+    }
+}
